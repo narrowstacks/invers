fa2af2a63d2c81401785ff3ec3e9158fa7b97470
narrowstacks <aaron@affords.art>
2025-11-27 00:14:50 -0800

    chore: bump package versions to 0.2.3 and update Homebrew formula
    - Updated package versions for invers-cli, invers-core, and invers-gui to 0.2.3 in Cargo.toml and Cargo.lock.
    - Bumped Homebrew formula version to 0.2.2 and updated SHA256 hashes for macOS and Linux builds.

--- Formula/invers.rb
+++ Formula/invers.rb
@@ -1,25 +1,25 @@
 class Invers < Formula
   desc "Professional-grade film negative to positive conversion tool"
   homepage "https://github.com/narrowstacks/invers"
-  version "0.2.0"
+  version "0.2.2"
   license any_of: ["MIT"]
 
   on_macos do
     on_intel do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-x86_64-apple-darwin.tar.gz"
-      sha256 "e80208109990139ed7c90d723ce34469969ad766c949aeafef851b53dff6d2e4"
+      sha256 "4abf53abb62c81467d86a21af608150cb5dfb408e70b1acff1b8d3d279796553"
     end
 
     on_arm do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-aarch64-apple-darwin.tar.gz"
-      sha256 "fcce8cd620ed6df9d6e407a939d3ca304072bf787fa3b6ff85312eeb0db129b6"
+      sha256 "fa962865b6784a6dc2203d1435593d3791d08008d5b46fdece4fb11277882b0e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/narrowstacks/invers/releases/download/v#{version}/invers-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e94dc8ca2ddaf1d4231ef8e6346d940a4ccfd46bfcfb3389ec81154ad0aacf24"
+      sha256 "0d91ce46e037e13630dbdeffdac36669c97d875bcd3db3568d18fe7af1657ef0"
     end
   end
 
